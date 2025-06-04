809376e27a5c83de108a33ca537a330ac6d3f2b1
santana-larissa <santanalarissa39@gmail.com>
2025-06-04 11:39:03 -0300

    contacorrente

--- src/model/Conta.ts
+++ src/model/Conta.ts
@@ -1,4 +1,4 @@
-export class Conta {
+export abstract class Conta {
 
     private _numero: number;
     private _agencia: number;
@@ -90,7 +90,6 @@ export class Conta {
         console.log("Tipo da Conta: " + tipo);
         console.log("Titular: " + this._titular);
         console.log("Saldo: " + this._saldo.toFixed(2));
-        console.log("*****************************************************\n\n")
 
     }
 
